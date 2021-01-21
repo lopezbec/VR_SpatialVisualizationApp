1d76b0e26d397bf96255d6db2d075e0604d6b4d5
Danalexanderc <31372021+Danalexanderc@users.noreply.github.com>
2021-01-21 12:16:48 -0500

    Center of mass V1

---
+++ ObjectBuilder/ObjectBuilder/Assets/CenterOfMass.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterOfMass : MonoBehaviour
+{
+	public GameObject[] wedges;
+	public GameObject center;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	private int delay = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(delay == 15){
+			delay++;
+			
+			bool anyWedgesActive = false;
+			
+			for(int i = 0; i < 12; i++)
+				if(wedges[i].activeInHierarchy){
+					anyWedgesActive	= true;
+				}
+				
+			center.SetActive(anyWedgesActive);
+		}
+		else if (delay < 15){
+			delay++;
+		}
+		
+    }
+}
---
+++ ObjectBuilder/ObjectBuilder/Assets/Scripts/CameraPositioner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositioner : MonoBehaviour
+{
+	public GameObject[] centers;
+	public Transform position;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	private int delay = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if(delay == 30){
+			delay++;
+			
+			for(int i = 0; i < 27; i++){
+				
+				if(centers[i].activeInHierarchy)
+					position.position += centers[i].GetComponent<Transform>().position;
+				
+			}
+			
+			//gameObject.GetComponent<Transform>();
+			
+		}
+		else if (delay < 30){
+			delay++;
+		}
+	 
+    }
+}
