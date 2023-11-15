11259f46a2c374c69d8a2e3fae1a49fe40a2b6f9
Aidanjm13 <110207064+Aidanjm13@users.noreply.github.com>
2023-11-14 22:24:35 -0500

    FileWriting Strides

--- ObjectViewer/Assets/Scripts/CollectData.cs
+++ ObjectViewer/Assets/Scripts/CollectData.cs
@@ -11,20 +11,17 @@ public class CollectData : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        List<playerAction> actions = new List<playerAction>();
+        actions = new List<PlayerAction>();
         DontDestroyOnLoad(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    class playerAction
-    {
-        string actionType;
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            actions.Add(new Click(Input.mousePosition));
+        }
     }
 
     public void loadFile(string name)
@@ -36,6 +33,77 @@ public class CollectData : MonoBehaviour
 
     public void UpdateFile()
     {
+
         //takes current object data and overwrites file
+    }
+
+    class PlayerData
+    {
+        List<PlayerAction> actions;
+        
+        public PlayerData()
+        {
+            actions = new List<PlayerAction>();
+        }
+    }
+
+    class PlayerAction
+    {
+        protected System.DateTime time;
+        //stores the time of the action
+    }
+
+    class Login : PlayerAction
+    {
+        public Login()
+        {
+            this.time = System.DateTime.Now;
+        }
+    }
+
+    class Click : PlayerAction
+    {
+        Vector3 mouseLocation;
+        public Click(Vector3 location)
+        {
+            this.time = System.DateTime.Now;
+            mouseLocation = location;
+        }
+    }
+
+    class ButtonPress : PlayerAction
+    {
+        string buttonName;
+        public ButtonPress(string button)
+        {
+            this.time = System.DateTime.Now;
+            buttonName = button;
+        }
+    }
+
+    class SceneChange : PlayerAction
+    {
+        string oldSceneName;
+        string newSceneName;
+        public SceneChange(string oldS, string newS)
+        {
+            oldSceneName = oldS;
+            newSceneName = newS;
+        }
+    }
+
+    class ChallengeSubmission : PlayerAction
+    {
+        string challengeName;
+        bool success;
+        int currentChallengeNum;
+        int totalNumChallenges;
+        public ChallengeSubmission(string name, bool correct, int chalNumber, int totalNum)
+        {
+            challengeName = name;
+            success = correct;
+            currentChallengeNum = chalNumber;
+            totalNumChallenges = totalNum;
+        }
     }
 }
