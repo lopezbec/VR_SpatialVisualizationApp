0860fb5715c337edcf01ec37141dd42d946ae4d6
MysticReaper743 <75450795+MysticReaper743@users.noreply.github.com>
2024-06-22 15:54:01 -0400

    Added changed GUI with arrow keys to manipulate reference object. Axes is moved above the WASD controls. Added starting default voxel named baseObject

---
+++ ObjectViewer/Assets/Scripts/ObjectViewer/CombinatorControlsUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombinatorControlsUI : MonoBehaviour
+{
+	
+	public Color normal, active;
+	public GameObject[] buttonObjects;
+	
+	private Image[] buttons = new Image[14];
+	
+	private KeyCode[] inputs = {KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.R, KeyCode.Space, KeyCode.Z, KeyCode.UpArrow, KeyCode.X, KeyCode.LeftArrow, KeyCode.DownArrow,  KeyCode.RightArrow };
+	
+	void Start() {
+		for (int i = 0; i < buttonObjects.Length; i++)
+		{
+			buttons[i] = buttonObjects[i].GetComponent<Image>();
+		}
+	}
+	
+    void Update()
+    {
+		for (int i = 0; i < buttonObjects.Length; i++)
+			if (Input.GetKey(inputs[i]))
+			{
+				buttons[i].color = active;
+			}
+			else
+			{
+				buttons[i].color = normal;
+			}
+    }
+}
